2fd23c89cd65f02ef1a5c9038823ff440b387d0f
Siddhi <ssiddhisonare26@gmail.com>
2025-11-27 22:38:32 +0530

    changed text in homepage

--- src/Component/Homepage.jsx
+++ src/Component/Homepage.jsx
@@ -5,7 +5,7 @@ import { FaSearch } from "react-icons/fa";
 export default function Homepage() {
   return (
     <div className="homepage-container">
-      <h1 className="homepage-title">Search here......</h1>
+      <h1 className="homepage-title">Thinking......</h1>
       <div className="homepage-search">
         <input type="text" placeholder="Type a message..." />
         <button>
